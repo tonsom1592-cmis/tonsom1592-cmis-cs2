8fc89acb0be7fbe803298e388b61364a4ac9e153
cmis <cmis@cmis-hs01.(none)>
2016-02-03 08:33:27 +0700

    Finished Assignment1

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,48 @@
 myName = "Tonsom"
+#Tells what my name is
 myAge = 18.06
+#Tells what my age is 
 myHeightInMeters = 1.63
-lengthOfOneSideofSquare = 3
-lengthOfOneSideofRectangle = 4
+#Tells what my height in meters is
+lengthOfOneSideOfSquare = 3
+#Tells how much a length of one side of a square is
+lengthOfOneSideOfRectangle = 4
+#Tells how much a length of one side of a rectangle is
 heightOfRectangle = 6
+#Tells height of a rectangle
 myAgeInMonths = myAge * 12
+#Tells my age in months
 yearsLeftToLive = 80 - myAge
+#Tells how many years I have left to live. Statistically
 myHeightInFeet = myHeightInMeters * 3.28
+#Tells my height in feet
 myHeightComparedToAverageInMeters = myHeightInMeters - 1.6 
+#Tells my height compared to the average height in meters
 areaOfSquare = 3 * 3
-halfVolumeOfCube = 
+#Tells area of a square
+halfVolumeOfCube = 3 ** 3/2.0
+#Tells the half volume of a cube
+areaOfRectangle = 4 ** 3/0.11
+#Tells the area of a rectangle
+print "My name is " + str(myName) + "."
+#prints my name in a sentence
+"I am " + str(myAge) + "years olds."
+#prints my age in a sentence
+"I am " + str(myHeightInMeters) + "meters tall."
+#prints my height in a sentence
+"My age in months is " + str(myAgeInMonths) + "months." 
+#prints my age in months in a sentence
+"I have " + str(yearsLeftToLive) + "years left to live."
+#prints the amount of years I have left to live statistically in a sentence 
+print "Length of one side of a square is " , (lengthOfOneSideOfSquare) , "."
+#prints length of one side of a square in a sentence
+"Length of one side of a rectangle is " , (lengthOfOneSideOfRectangle) , "."
+#prints length of one side of a rectangle in a sentence
+"Height of a rectangle is " , (heightOfRectangle) , "."
+#prints height of a rectangle in a sentence
+"Area of a square is " , (areaOfSquare) , "."
+#prints area of a square in a sentence
+"Area of a rectangle is " , (areaOfRectangle) , "."
+#prints area of a rectangle in a sentence
+print " ;) " * 10000
+#prints 10,000 winking faces
