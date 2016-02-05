f206f4b3772912b41652bea1c9c2b8e865bc0f03
cmis <cmis@cmis-hs01.(none)>
2016-02-05 13:35:26 +0700

    added typesoffish.py

---
+++ typesoffish.py
@@ -0,0 +1,25 @@
+def fish():
+	print"fish can swim"
+	print"fish breathe under water"
+	print"angel fish die easily"
+	print"Tunas taste great"
+	print"I love sushi" 
+fish()
+
+def I_am(action):
+	print "I am "+action+"ing"
+I_am("sleep")
+I_am("read")
+I_am("watch")
+I_am("eat")
+
+def she_has(thing1, thing2):
+	print "She has " + thing1
+	print "She also has " + thing2
+
+def describe(prop, factor):
+	degree = "very, " * factor
+	print "Your mom is " + degree + prop
+
+describe("nice", 3)
+describe("fun", 5)
