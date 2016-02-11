42b11dea0d444dd57351195ad4e9d7c4d9c305d1
cmis <cmis@cmis-hs01.(none)>
2016-02-11 13:35:39 +0700

    edited assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -18,28 +18,31 @@ myHeightInFeet = myHeightInMeters * 3.28
 #Tells my height in feet
 myHeightComparedToAverageInMeters = myHeightInMeters - 1.6 
 #Tells my height compared to the average height in meters
-areaOfSquare = 3 * 3
+areaOfSquare = lengthOfOneSideOfSquare * lengthOfOneSideOfSquare
 #Tells area of a square
-halfVolumeOfCube = 3 ** 3/2.0
+halfVolumeOfCube = lengthOfOneSideOfSquare ** lengthOfOneSideOfSquare/2.0
 #Tells the half volume of a cube
-areaOfRectangle = 4 ** 3/0.11
+areaOfRectangle = lengthOfOneSideOfRectangle ** lengthOfOneSideOfSquare/0.11
 #Tells the area of a rectangle
 print "My name is " + str(myName) + "." 
+#prints my name 
 print "I am " + str(myAge) + " years old."
-print "I am " + str(myHeightInMeters) + " meters tall." 
+#prints my age in years
+print "I am " + str(myHeightInMeters) + " meters tall."
+#prints my height 
 print "My age in months is " + str(myAgeInMonths) + " months." 
-#prints my strings
-"I have " + str(yearsLeftToLive) + "years left to live."
+#prints my age in months
+print "I have " + str(yearsLeftToLive) + "years left to live."
 #prints the amount of years I have left to live statistically in a sentence 
 print "Length of one side of a square is " , (lengthOfOneSideOfSquare ) , "."
 #prints length of one side of a square in a sentence
-"Length of one side of a rectangle is " , (lengthOfOneSideOfRectangle) , "."
+print "Length of one side of a rectangle is " , (lengthOfOneSideOfRectangle) , "."
 #prints length of one side of a rectangle in a sentence
-"Height of a rectangle is " , (heightOfRectangle) , "."
+print "Height of a rectangle is " , (heightOfRectangle) , "."
 #prints height of a rectangle in a sentence
-"Area of a square is " , (areaOfSquare) , "."
+print "Area of a square is " , (areaOfSquare) , "."
 #prints area of a square in a sentence
-"Area of a rectangle is " , (areaOfRectangle) , "."
+print "Area of a rectangle is " , (areaOfRectangle) , "."
 #prints area of a rectangle in a sentence
 print " ;) " * 10000
 #prints 10,000 winking faces
