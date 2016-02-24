ae5c0167fef1def60a820e3af365e46f5779bbba
cmis <cmis@cmis-hs01.(none)>
2016-02-24 10:10:31 +0700

    edited functions.py

--- functions.py
+++ functions.py
@@ -64,28 +64,46 @@ def center(a):
 center("Hello")
 #aligns hello to center of python
 def msgbox(a):
-	x =    """		  +---------+
-		  |  """+"Hello"+"""  |
-		  +---------+"""
-	print x
-	return"""		  +---------------+
-		  |  """+a+"""  |
-		  +---------------+"""
+	return "+" + (len(a) + int(4)) * "-" + "+\n" "|" + "  " + a + "  " + "|\n" "+" + (len(a) + int(4)) * "-" + "+"
 	
-print msgbox("I eat cats!")
-# displays the word Hello and I eat cats! in a box
+msgbox("Hello")
+msgbox("I eat cats!")
+# displays Hello and I eat cats! in a box
 
  a = add(4,3)
- s = sub(9,2)
- m = mul(4, 4)
- d = div(8,4)
- h = SecsToHours(86400,60,60) 
- c = Areaofcircle=(5)
- v = Volumeofsphere=(5)
- t = areaoftriangle(1,2,2.5)
+ b = add(5,9)
+print msgbox(str(a))
+print msgbox(str(b))
+ c = sub(9,2)
+ d = sub(7,3)
+ print msgbox(str(c))
+ print msgbox(str(d))
+ e = mul(4,4)
+ f = mul(6,7)
+ print msgbox(str(e))
+ print msgbox(str(f))
+ g = div(8,4)
+ h = div(4,2) 
+ print msgbox(str(g))
+ print msgbox(str(h))
+ i = SecsToHours(86400,60,60) 
+ j = SecsToHours(60000,60,60)
+ print msgbox(str(i))
+ print msgbox(str(j))
+ k = Areaofcircle=(5)
+ l = Areaofcircle=(9)
+ print msgbox(str(k))
+ print msgbox(str(l))
+ m = Volumeofsphere=(3)
+ n = Volumeofsphere =(8)
+ print msgbox(str(m))
+ print msgbox(str(n))
+ o = areaoftriangle(1,2,8)
+ p = areaoftriangle(3,5,7)
+ print msgbox(str(o))
+ print msgbox(str(p))
 #calls each of the functions
 
-print msg_box(a)
 
 
  
