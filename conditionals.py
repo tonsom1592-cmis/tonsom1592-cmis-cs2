e737c0ce29e753fc9e82a348c2246ffae6cf0906
cmis <cmis@cmis-hs01.(none)>
2016-03-01 10:55:35 +0700

    added conditionals

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+def  caughtInALandSlide():
+	print "No Escape from reality"
+def lookUpToTheSkiesAndSee():
+	print "I'm just a poor boy"
+def main():
+	isThisTheRealLife = True
+	IsThisJustFantasy = False
+
+	if isThisTheRealLife and orIsThisJustFantasy:
+		caughtInALandSlide()
+		lookUpToTheSkiesAndSee()
+
+	print "Okay, bye"
+	
+	
