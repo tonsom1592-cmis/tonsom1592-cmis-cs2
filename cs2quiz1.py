47aec40a9918a9d83667a8b31c5e3b1b3a61b2d4
cmis <cmis@cmis-hs01.(none)>
2016-03-02 13:36:58 +0700

    did the quiz

---
+++ cs2quiz1.py
@@ -0,0 +1,61 @@
+#Part 1: Terminology (15 points)
+#1 1pt) What is the symbol "=" used for?
+#The symbol "=" is used to assign a value. 
+#
+#
+#2 3pts) Write a technical definition for 'function'
+#A function is a sequence of instructions to perform a calculation. 
+#
+#
+#3 1pt) What does the keyword "return" do?
+#The keyword "return" is used to return a value 
+#
+#
+#4 5pts) We know 5 basic data types. Write the name for each one and provide two
+#   examples of each below
+#	1: int 
+# int('-23') 
+#int(-234+23)
+#	2: str
+#str('Hello World')
+#str('Python')
+#	3: float
+#float('0.0')
+#float('0.1')
+#	4: tuple 
+#tuple('Tonsom',)
+#tuple(18,)
+#	5: bool
+#print a == 6
+#print not a == 7
+#5 2pts) What is the difference between a "function definition" and a 
+#        "function call"?
+# A function definition defines a squence of instructions while a function 
+#call, calls the defined squence of instructions 
+#
+#
+#6 3pts) What are the 3 phases that every computer program has? What happens in
+#        each of them
+#	1: Assigning value to to a function. A value is assigned to a function
+#	2:Defining the function. The function is defined
+#	3:Calling the function. The function gets called. 
+#
+#Part 2: Programming (25 points)
+#Write a program that asks the user for the areas of 3 circles.
+#It should then calculate the diameter of each and the sum of the diameters 
+#of the 3 circles.
+#Finally, it should produce output like this:
+def areaOfCircle(a, b, c):
+	return a + b + c
+
+def output(c1
+#please dont grade yet, im not done :(
+
+#Circle  Diameter
+#c1      ...
+#c2      ...
+#c3      ...
+#TOTALS  ...
+
+# Hint: Radius is the square root of the area divided by pi
+
