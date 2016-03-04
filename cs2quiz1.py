728d60007186fc1123fdfbd84c86fe4db4536267
cmis <cmis@cmis-hs01.(none)>
2016-03-04 08:39:16 +0700

    halfway graded

--- cs2quiz1.py
+++ cs2quiz1.py
@@ -1,15 +1,15 @@
 #Part 1: Terminology (15 points)
 #1 1pt) What is the symbol "=" used for?
 #The symbol "=" is used to assign a value. 
-#
+#1 Correct
 #
 #2 3pts) Write a technical definition for 'function'
 #A function is a sequence of instructions to perform a calculation. 
-#
+# 3 Correct
 #
 #3 1pt) What does the keyword "return" do?
 #The keyword "return" is used to give the final output of a function, and is used inside a function as a statement.
-#
+#1 Correct
 #
 #4 5pts) We know 5 basic data types. Write the name for each one and provide two
 #   examples of each below
@@ -18,15 +18,17 @@
 #	3: float = 5.5, 5.76
 #	4: tuple = (" My "," name "," is "," Tonsom ") , (" I "," am "," 17 "," years "," old ")
 #	5: bool = True , False
+#5 Correct
 #5 2pts) What is the difference between a "function definition" and a 
 #        "function call"?
 # A function definition is naming a function, and calculating the statements. #Meanwhile, a function call is calling a defined function to perform a #calculation, and present the result to its caller. 
+# 2Correct
 #6 3pts) What are the 3 phases that every computer program has? What happens in
 #        each of them
 #	1: Input- The computer receives commands
 #	2: Process- Uses the commands and performs a caluculation
 #	3: Output- Presents the results of the calculations
-#
+#3 Correct
 #Part 2: Programming (25 points)
 #Write a program that asks the user for the areas of 3 circles.
 #It should then calculate the diameter of each and the sum of the diameters 
