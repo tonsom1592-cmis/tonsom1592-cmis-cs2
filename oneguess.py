b9799807bfb93d146bd5dbd3b531af1464149a0b
cmis <cmis@cmis-hs09.(none)>
2016-03-21 10:52:34 +0700

    oneguess.py

--- oneguess.py
+++ oneguess.py
@@ -1,5 +1,5 @@
 import random
-def numberRange(number1, number2)
+def numberRange(number1, number2):
 	return random.randint(number1, number2)
 
 def resultbyuser(result,usersGuess):
@@ -15,5 +15,16 @@ def main():
 	number2 = int(raw_input("What is the maximum number?"))
 	print "I'm thinking of a number from {} to {}".format(number1 , number2)
 	usersGuess = int(raw_input("What do you think it is?:"))
-	result = int
+	result = int(numberRange(number1,number2))
+	wrong = resultbyuser(result,usersGuess)
+	if result == usersGuess:
+		print """The number was {}
+Your guess was {}
+You are correct! I don't know how but woowowoowowoowowowowowowoowowowowowowoowowowowowoowowow, you're cool.""".format(result,usersGuess)
+	else:
+		print """The number was {}
+Your guess was {}
+{}""".format(result,usersGuess,wrong)
+
+main()
 
