04881c70e9e724394035af51cdd2a4b31fed1aaa
cmis <cmis@cmis-hs09.(none)>
2016-03-21 10:33:06 +0700

    edited simpleprogram

--- simpleprogram.py
+++ simpleprogram.py
@@ -2,18 +2,18 @@ def multiply(x, y):
 	z = x * y
 	return z
 	
-def output(name, x, y, z):
+def output(name, x, y, z, someone):
 	return """
 Hello {}
-Did you know:
-{} * {} = {}
-""".format(name, x, y, z,)
+Because you were born on {} of the month {} you have {} days left until {} finds your salvation. 
+""".format(name, x, y, z, someone)
 
 def main():
 	name = raw_input("Type your name: ") 
-	x = raw_input("Type a number: ")
-	y = raw_input("Type another number: ")
+	x = raw_input("Type the day you were born: ")
+	y = raw_input("Type the month you were born as a number: ")
 	z = multiply(int(x), int(y))
-	print output(name, x, y, z)
+	someone = raw_input("Type a famous person: ")
+	print output(name, x, y, z, someone)
 
 main() 
