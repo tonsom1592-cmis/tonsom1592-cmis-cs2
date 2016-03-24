e8513d18e774aef6f60bacb2a31d649f8d9d1f90
cmis <cmis@cmis-hs09.(none)>
2016-03-24 07:54:14 +0700

    finished the assignment

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,19 +1,19 @@
 def multiply(x, y):
-	z = x * y
-	return z
+	daysLeft = dayBorn * monthBorn
+	return daysLeft
 	
-def output(name, x, y, z, someone):
+def output(name, dayBorn, monthBorn, daysLeft, someone):
 	return """
 Hello {}
 Because you were born on {} of the month {} you have {} days left until {} finds your salvation. 
-""".format(name, x, y, z, someone)
+""".format(name, dayBorn, monthBorn, daysLeft, someone)
 
 def main():
 	name = raw_input("Type your name: ") 
-	x = raw_input("Type the day you were born: ")
-	y = raw_input("Type the month you were born as a number: ")
-	z = multiply(int(x), int(y))
+	dayBorn = raw_input("Type the day you were born: ")
+	monthBorn = raw_input("Type the month you were born as a number: ")
+	daysLeft = multiply(int(x), int(y))
 	someone = raw_input("Type a famous person: ")
-	print output(name, x, y, z, someone)
+	print output(name, dayBorn, monthBorn, daysLeft, someone)
 
 main() 
