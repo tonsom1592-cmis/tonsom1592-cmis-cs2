8044e95cd0a966248e92e3858aeadd9deda8a713
cmis <cmis@cmis-hs23.(none)>
2016-04-21 10:55:15 +0700

    added recursion

---
+++ recursion.py
@@ -0,0 +1,19 @@
+def countdown(n):
+	if n <= 0:
+		print "Blastoff!"
+	else:
+		print n
+		countdown(n-1)
+def countup(n):
+	
+
+
+
+def main():
+	countup(-10)
+	countup(0)
+	countup(10)
+
+	return
+
+main()
