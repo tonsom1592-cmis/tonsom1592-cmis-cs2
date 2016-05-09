d9ac7d0b35460498b876b8e56ac46f706e534253
cmis <cmis@cmis-hs11.(none)>
2016-05-09 08:40:43 +0700

    edited conditionals

--- conditionals.py
+++ conditionals.py
@@ -1,15 +1,30 @@
+import random
+import math
+print "Type the letter of the correct song lyrics: "
+
 def  caughtInALandSlide():
-	print "No Escape from reality"
-def lookUpToTheSkiesAndSee():
-	print "I'm just a poor boy"
+	print "Caught in a landslide"
+	a = "a: I took a pill so Avicii would think I'm cool"
+	b = "b: No escape from reality"
+	c = "c: Lately, I've been, I've been losing sleep"
+	print a
+	print b
+	print c
+
+	userinput = raw_input("Pick a,b or c:")  
+	if userinput == "b" : 
+		print "You are correct!"
+	else:
+		print "That's the wrong lyrics :("
+
 def main():
-	isThisTheRealLife = True
-	IsThisJustFantasy = False
+	caughtInALandSlide()
+def greenDay():
+	print "Greenday's __ Guns"
+	number = raw_input("Insert the correct number of the song title")
+	if number == "21": 
+		print "You are correct"
+	elif number <= 21 
 
-	if isThisTheRealLife and orIsThisJustFantasy:
-		caughtInALandSlide()
-		lookUpToTheSkiesAndSee()
 
-	print "Okay, bye"
-	
-	
+main()
