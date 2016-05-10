2c024747f1712d827a957b27322f4baf0d5acb37
cmis <cmis@cmis-hs11.(none)>
2016-05-10 10:54:19 +0700

    edited the game

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,9 @@
 import random
 import math
-print "Type the letter of the correct song lyrics: "
-
+print """ Welcome to The Interactive Music Game!
+	You will answer questions relating to music. Let's begin: """
 def  caughtInALandSlide():
-	print "Caught in a landslide"
+	print "Type the choice with the correct song lyrics: Caught in a landslide _______"
 	a = "a: I took a pill so Avicii would think I'm cool"
 	b = "b: No escape from reality"
 	c = "c: Lately, I've been, I've been losing sleep"
@@ -11,20 +11,55 @@ def  caughtInALandSlide():
 	print b
 	print c
 
-	userinput = raw_input("Pick a,b or c:")  
+	userinput = raw_input("Pick a, b or c: ")  
 	if userinput == "b" : 
 		print "You are correct!"
 	else:
 		print "That's the wrong lyrics :("
 
-def main():
-	caughtInALandSlide()
 def greenDay():
 	print "Greenday's __ Guns"
-	number = raw_input("Insert the correct number of the song title")
+	number = raw_input("Insert the correct number of the song title: ")
 	if number == "21": 
 		print "You are correct"
-	elif number <= 21 
+		print "Greenday's {} Guns".format(number)
+	elif number < "21":
+		print "That's too low"
+	elif number > "21":
+		print "That's too high"
 
 
+def britneySpeares():
+	number = raw_input("What year did Britney Speares have her famous meltdown?: ")
+	if number == "2007":
+		print "That's right! May we never forget"
+	elif number < "2007":
+		print "That's too early. She was still sane back then"
+	elif number > "2007":
+		print "That's too late. She bounced back."
+
+def sweetDreams():
+	print "Who originally wrote Sweet Dreams?: "
+	a = "a: Eurythmics"
+	b = "b: Marylin Manson"
+	c = "c: Emily Brown"
+	print a
+	print b
+	print c
+
+	userinput = raw_input("Pick a, b or c: ")  
+	if userinput == "a" : 
+		print "You are correct!"
+	else:
+		print "You are incorrect"
+
+	
+def main(): 
+	caughtInALandSlide()
+	greenDay()
+	britneySpeares()
+	sweetDreams()
+	
 main()
+
+
