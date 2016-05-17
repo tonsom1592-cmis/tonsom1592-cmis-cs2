f0b60801b59939c8d87f3dafa8eb0d4a0a59541d
cmis <cmis@cmis-hs11.(none)>
2016-05-17 13:11:19 +0700

    did the quiz not done yet

---
+++ cs3quiz.py
@@ -0,0 +1,63 @@
+#Section 1: Terminology
+# 1) What is a recursive function?
+# A recursive function is a function that calls itself 
+#
+#
+# 2) What happens if there is no base case defined in a recursive function?
+# The program would keep calling itself and cause a stack overflow 
+#
+#
+# 3) What is the first thing to consider when designing a recursive function?
+# What will go in a base case
+#
+#
+# 4) How do we put data into a function call?
+# Indent it then make a variable
+#
+# 
+# 5) How do we get data out of a function call?
+# Make it do something then call the function. 
+#
+#
+
+#Section 2: Reading
+# Read the following function definitions and function calls.
+# Then determine the values of the variables q1-q20.
+
+#a1 = 
+#a2 =
+#a3 = 
+
+#b1 = 
+#b2 = 
+#b3 = 
+
+#c1 = 
+#c2 =
+#c3 = 
+
+#d1 = 5
+#d2 = 7
+#d3 = 5
+
+#Section 3: Programming
+#Write a script that asks the user to enter a series of numbers.
+#When the user types in nothing, it should return the average of all the odd numbers
+#that were typed in. 
+#In your code for the script, add a comment labeling the base case  on the line BEFORE the base case.
+#Also add a comment label BEFORE the recursive case.
+#It is NOT NECESSARY to print out a running total with each user input.
+def check(numberR,averagE):
+	if int(numbeR) == :
+		user(averagE)
+	elif 
+
+def user():
+	numbeR = raw_input("Next number:")
+	if numbeR == "": 
+		print "The average of your numbers was ()".format()
+	else:
+		average = int(numbeR)/2
+		user()
+
+def main():
