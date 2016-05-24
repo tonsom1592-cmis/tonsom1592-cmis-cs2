be547b393f2860c80774ad11b90e48aeed62e633
cmis <cmis@cmis-hs11.(none)>
2016-05-24 13:35:32 +0700

    took notes

---
+++ whileloops.py
@@ -0,0 +1,32 @@
+#def countup(x): 
+#	while x >= 0:
+#		print x
+#		x -= 1
+#countup(11)
+
+#def countdown(a):
+#	while a <= 20:
+#		print a
+#		a += 1
+#countdown(11)
+
+def count(b):
+	while b > 0 or b < 0:
+	print b
+	b -= 1
+
+count(-12)
+
+
+#x = 10
+#while x >= 0:
+#	print x
+#	x -= 1
+
+#only one loop brah
+#while True:
+#	Print "something"
+#while False:
+#	Print "Something" 
+#nothing will happen
+
