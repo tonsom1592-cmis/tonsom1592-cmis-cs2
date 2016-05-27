f0870b95c4c5f9f173c8f838b60e754895f824ed
cmis <cmis@cmis-hs11.(none)>
2016-05-27 10:55:35 +0700

    edited

--- whileloops.py
+++ whileloops.py
@@ -1,3 +1,36 @@
+#def addodds(n):
+#	sum = 0
+#	if n < 0:
+#		while n < 0:
+#			if n % 2 == 1:
+#				sum += n
+
+#			n -= 1
+#	elif n < 0:
+#		while < 0:
+#			if n % 2 == 1:
+#				sum += n
+#			n += 1
+#		return sum
+
+def grid(w, h):
+	out = ""
+	out += "."*w
+	
+	
+		
+
+
+
+#def countfrom(a,b):
+#	while a <= b:
+#		print a
+#		a += 1
+#	while a >= b:
+#		print a
+#		a -= 1
+#countfrom(10,8)
+
 #def countup(x): 
 #	while x >= 0:
 #		print x
@@ -10,12 +43,14 @@
 #		a += 1
 #countdown(11)
 
-def count(b):
-	while b > 0 or b < 0:
-	print b
-	b -= 1
-
-count(-12)
+#def count(b):
+#	while b > 0:
+#		print b
+#		b -= 1
+#	while b < 0:
+#		print b
+#		b += 1
+#count(-12)
 
 
 #x = 10
